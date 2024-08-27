e777d9389efa22d10d93353a70323f76ae42cd0a
Matus <matuskocik@gmail.com>
2024-08-27 21:28:40 +0200

    update models

--- finance/models.py
+++ finance/models.py
@@ -1,25 +1,76 @@
 from django.db import models
+from django_cryptography.fields import encrypt
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
 
-class Income(models.Model): # Príjem
+
+class BaseModel(models.Model):
+    name = models.CharField(verbose_name=_("Name"), max_length=64)
+    description = models.TextField(verbose_name=_("Description"), blank=True, null=True)
+    
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self.name
+
+
+class IncomePlan: # Plánovaný príjem
     pass
 
-class IncomeCategory(models.Model): # Kategória príjmu
+
+class Income(BaseModel): # Príjem
+    amount = models.DecimalField(verbose_name=_("Amount"), max_digits=12, decimal_places=2)
+    payment_date = models.DateField(verbose_name=_("Payment Date"), default=now)
+    source = encrypt(models.ForeignKey("FinancialSource", verbose_name=_("Source"), on_delete=models.CASCADE))
+    category = encrypt(models.ForeignKey("IncomeCategory", verbose_name=_("Category"),  on_delete=models.CASCADE))
+
+
+class ExpensePlan: # Plánovaný výdavok
     pass
 
-class Expense(models.Model): # Výdavok
+
+class Expense(BaseModel): # Výdavok
+    amount = models.DecimalField(verbose_name=_("Amount"), max_digits=12, decimal_places=2)
+    payment_date = models.DateField(verbose_name=_("Payment Date"), default=now)
+    source = models.ForeignKey("FinancialSource", verbose_name=_("Source"), on_delete=models.CASCADE)
+    category = models.ForeignKey("ExpenseCategory", verbose_name=_("Category"), on_delete=models.CASCADE)
+    is_paid = models.BooleanField(verbose_name=_("Is Paid"), default=False)
+
+
+class BankAccount(BaseModel): # Bankový účet
+    bank_name = models.CharField(verbose_name=_("Bank Name"), max_length=64)
+    iban = encrypt(models.CharField(verbose_name=_("IBAN"), max_length=34))
+    balance = encrypt(models.DecimalField(verbose_name=_("Balance"), max_digits=12, decimal_places=2))
+    currency = models.CharField(verbose_name=_("Currency"), max_length=16)
+    account_type = models.CharField(verbose_name=_("Account Type"), max_length=64)
+
+
+class InvestmentPlan: # Plánovaná investícia
     pass
 
-class ExpenseCategory(models.Model): # Kategória výdavku
+
+class Investment(BaseModel): # Investícia
+    amount = models.DecimalField(verbose_name=_("Amount"), max_digits=12, decimal_places=2)
+    investment_date = models.DateField(verbose_name=_("Investment Date"), auto_now=False, auto_now_add=False)
+    is_paid = models.BooleanField(verbose_name=_("Is Paid"), default=False)
+
+
+class IncomeCategory(BaseModel): # Kategória príjmu
     pass
 
-class FinancialSource(models.Model): # Zdroj
+
+class ExpenseCategory(BaseModel): # Kategória výdavku
     pass
 
-class Investment(models.Model): # Investícia
+
+class FinancialSource(BaseModel): # Zdroj
     pass
 
-class Debt(models.Model): # Dlh
+
+class Debt(BaseModel): # Dlh
     pass
 
-class Claim(models.Model): # Pohľadávka
+
+class Claim(BaseModel): # Pohľadávka
     pass
--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$9w&6s_9c4k+4q17wgcjpom!w+3*@2s(4&imn&5ifuoahl3&1@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
