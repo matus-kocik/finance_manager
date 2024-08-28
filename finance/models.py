a87bbf09803a827e13a69b699fd6c95b27d2fd14
Matus <matuskocik@gmail.com>
2024-08-28 06:28:38 +0200

    update models

--- finance/models.py
+++ finance/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 class BaseModel(models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=64)
-    description = models.TextField(verbose_name=_("Description"), blank=True, null=True)
+    description = models.TextField(verbose_name=_("Description"), blank=True)
     
     class Meta:
         abstract = True
@@ -16,23 +16,33 @@ class BaseModel(models.Model):
 
 
 class IncomePlan: # Plánovaný príjem
-    pass
+    planned_amount = encrypt(models.DecimalField(verbose_name=_("Planned Amount"), max_digits=12, decimal_places=2, blank=True, default=0))
+    planned_payment_date = models.DateField(verbose_name=_("Planned Payment Date"))
+    planned_hours = models.DecimalField(verbose_name=_("Planned Hours"), max_digits=6, decimal_places=2, blank=True, default=0)
+    planned_hourly_rate = encrypt(models.DecimalField(verbose_name=_("Planned Hourly Rate"), max_digits=5, decimal_places=2, blank=True, default=0))
+    source = encrypt(models.ForeignKey("FinancialSource", verbose_name=_("Source"), on_delete=models.CASCADE))
+    category = encrypt(models.ForeignKey("IncomeCategory", verbose_name=_("Category"),  on_delete=models.CASCADE))
 
 
 class Income(BaseModel): # Príjem
-    amount = models.DecimalField(verbose_name=_("Amount"), max_digits=12, decimal_places=2)
-    payment_date = models.DateField(verbose_name=_("Payment Date"), default=now)
+    actual_amount = encrypt(models.DecimalField(verbose_name=_("Actual Amount"), max_digits=12, decimal_places=2, blank=True, default=0))
+    actual_payment_date = models.DateField(verbose_name=_("Actual Payment Date"), default=now)
+    actual_hours = models.DecimalField(verbose_name=_("Actual Hours"), max_digits=6, decimal_places=2, blank=True, default=0)
+    actual_hourly_rate = encrypt(models.DecimalField(verbose_name=_("Actual Hourly Rate"), max_digits=5, decimal_places=2, blank=True, default=0))
     source = encrypt(models.ForeignKey("FinancialSource", verbose_name=_("Source"), on_delete=models.CASCADE))
     category = encrypt(models.ForeignKey("IncomeCategory", verbose_name=_("Category"),  on_delete=models.CASCADE))
 
 
 class ExpensePlan: # Plánovaný výdavok
-    pass
+    planned_amount = models.DecimalField(verbose_name=_("Planned Amount"), max_digits=12, decimal_places=2, blank=True, default=0)
+    planned_payment_date = models.DateField(verbose_name=_("Planned Payment day"))
+    source = models.ForeignKey("FinancialSource", verbose_name=_("Source"), on_delete=models.CASCADE)
+    category = models.ForeignKey("ExpenseCategory", verbose_name=_("Category"), on_delete=models.CASCADE)
 
 
 class Expense(BaseModel): # Výdavok
-    amount = models.DecimalField(verbose_name=_("Amount"), max_digits=12, decimal_places=2)
-    payment_date = models.DateField(verbose_name=_("Payment Date"), default=now)
+    actual_amount = models.DecimalField(verbose_name=_("Actual Amount"), max_digits=12, decimal_places=2, blank=True, default=0)
+    actual_payment_date = models.DateField(verbose_name=_("Actual Payment Date"), default=now)
     source = models.ForeignKey("FinancialSource", verbose_name=_("Source"), on_delete=models.CASCADE)
     category = models.ForeignKey("ExpenseCategory", verbose_name=_("Category"), on_delete=models.CASCADE)
     is_paid = models.BooleanField(verbose_name=_("Is Paid"), default=False)
