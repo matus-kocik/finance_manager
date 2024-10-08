783104677eb11945223a1740803786dd2da7e4b6
Matus <matuskocik@gmail.com>
2024-10-08 06:18:10 +0200

    create first pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,38 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const Logout = () => {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+};
+
+const RegisterAndLogout = () => {
+  localStorage.clear();
+  return <Register />;
+};
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
